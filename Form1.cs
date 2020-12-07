9ff9713f0bd28f44abb8553d0fc167c11e5f7ae7
Jonatan <46535318+jrog24@users.noreply.github.com>
2020-12-07 14:02:02 +0100

    first build of teapicker

---
+++ Form1.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TeaPicker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RandomTea(string TeaType)
+        {
+            var teaList = new List<string>();
+
+            int lineCount = 0;
+
+            using (var teaReader = new StreamReader(TeaType))
+            {
+                string line;
+
+                while ((line = teaReader.ReadLine()) != null)
+                {
+                    teaList.Add(line);
+                    lineCount++;
+                }
+            }
+            string[] results= teaList.ToArray();
+            Random rnd = new Random();
+            int tea= rnd.Next(0, lineCount);
+            resultsTextBox.Text = results[tea];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RandomTea("Path to first kind of tea");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RandomTea("Path to second kind of tea");
+        }
+    }
+}
