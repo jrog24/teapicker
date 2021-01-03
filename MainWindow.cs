de8169b10b9f27fcb894d2195d0c2358e3de43f0
Jonatan <46535318+jrog24@users.noreply.github.com>
2021-01-03 13:36:53 +0100

    Update MainWindow.cs

--- MainWindow.cs
+++ MainWindow.cs
@@ -5,9 +5,9 @@ using System.IO;
 
 namespace TeaPicker
 {
-    public partial class Form1 : Form
+    public partial class MainWindow : Form
     {
-        public Form1()
+        public MainWindow()
         {
             InitializeComponent();
         }
